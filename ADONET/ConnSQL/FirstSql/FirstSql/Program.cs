f2d7c8e04af215289010886deabfb97e8e9c23c1
wdwoodee <wdwoodee@163.com>
2016-04-12 22:07:21 +0800

    add vs2013 ado dot net

---
+++ ADONET/ConnSQL/FirstSql/FirstSql/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataDir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dataDir.EndsWith(@"\bin\Debug\")
+                || dataDir.EndsWith(@"\bin\Release\"))
+            {
+                dataDir = System.IO.Directory.GetParent(dataDir).Parent.Parent.FullName;
+                AppDomain.CurrentDomain.SetData("DataDirectory", dataDir);
+            }
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            conn.Open();
+            Console.WriteLine("DataBase connection success!");
+            Console.ReadKey();
+        }
+    }
+}
