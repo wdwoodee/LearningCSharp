71f187bf1677b9f1dca31ebe4199351017adeca2
wdwoodee <wdwoodee@163.com>
2016-06-04 22:51:13 +0800

    add new project

---
+++ ADONET/WinFormSQL/WinFormSQL/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WinFormSQL
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labUsername = new System.Windows.Forms.Label();
+            this.labPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labUsername
+            // 
+            this.labUsername.AutoSize = true;
+            this.labUsername.Location = new System.Drawing.Point(71, 70);
+            this.labUsername.Name = "labUsername";
+            this.labUsername.Size = new System.Drawing.Size(53, 12);
+            this.labUsername.TabIndex = 0;
+            this.labUsername.Text = "Username";
+            // 
+            // labPassword
+            // 
+            this.labPassword.AutoSize = true;
+            this.labPassword.Location = new System.Drawing.Point(71, 120);
+            this.labPassword.Name = "labPassword";
+            this.labPassword.Size = new System.Drawing.Size(53, 12);
+            this.labPassword.TabIndex = 1;
+            this.labPassword.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(158, 70);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 21);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(158, 120);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 21);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(183, 178);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 249);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.labPassword);
+            this.Controls.Add(this.labUsername);
+            this.Name = "LoginForm";
+            this.Text = "LoginTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labUsername;
+        private System.Windows.Forms.Label labPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ ADONET/WinFormSQL/WinFormSQL/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormSQL
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void IncErrorTimes()
+        {
+            string dataDir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dataDir.EndsWith(@"\bin\Debug\")
+                || dataDir.EndsWith(@"\bin\Release\"))
+            {
+                dataDir = System.IO.Directory.GetParent(dataDir).Parent.Parent.FullName;
+                AppDomain.CurrentDomain.SetData("DataDirectory", dataDir);
+            }
+            string connString = ConfigurationManager.ConnectionStrings["connectString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand updateCmd = conn.CreateCommand())
+                {
+                    updateCmd.CommandText = "update T_User Set ErrorTimes=ErrorTimes+1 where Name=@UName";
+                    updateCmd.Parameters.Add(new SqlParameter("UName", txtUsername.Text));
+                    updateCmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void ResetErrorTimes()
+        {
+            string dataDir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dataDir.EndsWith(@"\bin\Debug\")
+                || dataDir.EndsWith(@"\bin\Release\"))
+            {
+                dataDir = System.IO.Directory.GetParent(dataDir).Parent.Parent.FullName;
+                AppDomain.CurrentDomain.SetData("DataDirectory", dataDir);
+            }
+            string connString = ConfigurationManager.ConnectionStrings["connectString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand updateCmd = conn.CreateCommand())
+                {
+                    updateCmd.CommandText = "update T_User Set ErrorTimes=0 where Name=@UName";
+                    updateCmd.Parameters.Add(new SqlParameter("UName", txtUsername.Text));
+                    updateCmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string dataDir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dataDir.EndsWith(@"\bin\Debug\")
+                || dataDir.EndsWith(@"\bin\Release\"))
+            {
+                dataDir = System.IO.Directory.GetParent(dataDir).Parent.Parent.FullName;
+                AppDomain.CurrentDomain.SetData("DataDirectory", dataDir);
+            }
+            string connString = ConfigurationManager.ConnectionStrings["connectString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from T_User where Name=@UserN";
+                    cmd.Parameters.Add(new SqlParameter("UserN", txtUsername.Text));
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            int errorTimes = reader.GetInt32(reader.GetOrdinal("ErrorTimes"));
+                            if (errorTimes > 3)
+                            {
+                                MessageBox.Show("Login more than there time, can't login again.");
+                                return;
+                            }
+                            string dbPassword = reader.GetString(reader.GetOrdinal("Password"));
+                            if (dbPassword == txtPassword.Text)
+                            {
+                                ResetErrorTimes();
+                                MessageBox.Show("Login Successfully.");
+                            }
+                            else
+                            {
+                                IncErrorTimes();
+                                MessageBox.Show("Login Failed, please try again.");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("User is not exist.");
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
