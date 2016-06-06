814c1fc4d15fe4f4d749bb688c964c4d794d33cd
wdwoodee <wdwoodee@163.com>
2016-06-07 01:10:19 +0800

    add db add complete coding.

--- ADONET/ProvinceCityAreaQuery/ProvinceCityAreaQuery/Form1.Designer.cs
+++ ADONET/ProvinceCityAreaQuery/ProvinceCityAreaQuery/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbProvince = new System.Windows.Forms.ComboBox();
+            this.cmbCity = new System.Windows.Forms.ComboBox();
+            this.cmbArea = new System.Windows.Forms.ComboBox();
+            this.labProvince = new System.Windows.Forms.Label();
+            this.labCity = new System.Windows.Forms.Label();
+            this.labArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbProvince
+            // 
+            this.cmbProvince.DisplayMember = "provinceName";
+            this.cmbProvince.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProvince.FormattingEnabled = true;
+            this.cmbProvince.Location = new System.Drawing.Point(84, 72);
+            this.cmbProvince.Name = "cmbProvince";
+            this.cmbProvince.Size = new System.Drawing.Size(121, 20);
+            this.cmbProvince.TabIndex = 0;
+            this.cmbProvince.SelectedIndexChanged += new System.EventHandler(this.cmbProvince_SelectedIndexChanged);
+            // 
+            // cmbCity
+            // 
+            this.cmbCity.DisplayMember = "cityName";
+            this.cmbCity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCity.FormattingEnabled = true;
+            this.cmbCity.Location = new System.Drawing.Point(272, 72);
+            this.cmbCity.Name = "cmbCity";
+            this.cmbCity.Size = new System.Drawing.Size(121, 20);
+            this.cmbCity.TabIndex = 1;
+            this.cmbCity.SelectedIndexChanged += new System.EventHandler(this.cmbCity_SelectedIndexChanged);
+            // 
+            // cmbArea
+            // 
+            this.cmbArea.DisplayMember = "areaName";
+            this.cmbArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbArea.FormattingEnabled = true;
+            this.cmbArea.Location = new System.Drawing.Point(462, 72);
+            this.cmbArea.Name = "cmbArea";
+            this.cmbArea.Size = new System.Drawing.Size(121, 20);
+            this.cmbArea.TabIndex = 2;
+            // 
+            // labProvince
+            // 
+            this.labProvince.AutoSize = true;
+            this.labProvince.Location = new System.Drawing.Point(25, 75);
+            this.labProvince.Name = "labProvince";
+            this.labProvince.Size = new System.Drawing.Size(53, 12);
+            this.labProvince.TabIndex = 3;
+            this.labProvince.Text = "Province";
+            // 
+            // labCity
+            // 
+            this.labCity.AutoSize = true;
+            this.labCity.Location = new System.Drawing.Point(237, 75);
+            this.labCity.Name = "labCity";
+            this.labCity.Size = new System.Drawing.Size(29, 12);
+            this.labCity.TabIndex = 4;
+            this.labCity.Text = "City";
+            // 
+            // labArea
+            // 
+            this.labArea.AutoSize = true;
+            this.labArea.Location = new System.Drawing.Point(427, 75);
+            this.labArea.Name = "labArea";
+            this.labArea.Size = new System.Drawing.Size(29, 12);
+            this.labArea.TabIndex = 5;
+            this.labArea.Text = "Area";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 198);
+            this.Controls.Add(this.labArea);
+            this.Controls.Add(this.labCity);
+            this.Controls.Add(this.labProvince);
+            this.Controls.Add(this.cmbArea);
+            this.Controls.Add(this.cmbCity);
+            this.Controls.Add(this.cmbProvince);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbProvince;
+        private System.Windows.Forms.ComboBox cmbCity;
+        private System.Windows.Forms.ComboBox cmbArea;
+        private System.Windows.Forms.Label labProvince;
+        private System.Windows.Forms.Label labCity;
+        private System.Windows.Forms.Label labArea;
     }
 }
 
--- ADONET/ProvinceCityAreaQuery/ProvinceCityAreaQuery/Form1.cs
+++ ADONET/ProvinceCityAreaQuery/ProvinceCityAreaQuery/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,134 @@ namespace ProvinceCityAreaQuery
 {
     public partial class Form1 : Form
     {
+        string connString = ConfigurationManager.ConnectionStrings["connectString"].ConnectionString;           
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            Person p1 = new Person();
+            p1.name = "tom";
+            p1.age = 20;
+            //MessageBox.Show(p1.ToString());
+
+            Person p2 = new Person();
+            p2.name = "jim";
+            p2.age = 22;
+
+            cmbProvince.Items.Add(p1);
+            cmbProvince.Items.Add(p2);
+
+            return;
+             */
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from province";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ProvinceItem item = new ProvinceItem();
+                            item.provinceName = reader.GetString(reader.GetOrdinal("name"));
+                            item.provinceCode = reader.GetString(reader.GetOrdinal("code"));
+                            cmbProvince.Items.Add(item);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void cmbProvince_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ProvinceItem item = (ProvinceItem)cmbProvince.SelectedItem;
+            string proCode = item.provinceCode;
+
+            cmbCity.Items.Clear();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from city where provincecode=@provinceCode";
+                    cmd.Parameters.Add(new SqlParameter("provinceCode", proCode));
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            CityItem cityItem = new CityItem();
+                            cityItem.cityName = reader.GetString(reader.GetOrdinal("name"));
+                            cityItem.cityCode = reader.GetString(reader.GetOrdinal("code"));
+                            cmbCity.Items.Add(cityItem);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void cmbCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CityItem item = (CityItem)cmbCity.SelectedItem;
+            string cityCode = item.cityCode;
+
+            cmbArea.Items.Clear();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from area where citycode=@cityCode";
+                    cmd.Parameters.Add(new SqlParameter("cityCode", cityCode));
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            AreaItem areaItem = new AreaItem();
+                            areaItem.areaName = reader.GetString(reader.GetOrdinal("name"));
+                            areaItem.code = reader.GetString(reader.GetOrdinal("code"));
+                            cmbArea.Items.Add(areaItem);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class ProvinceItem
+    {
+        public int id { get; set; }
+        public string provinceName { get; set; }
+        public string provinceCode { get; set; }
+    }
+
+    public class CityItem
+    {
+        public int id { get; set; }
+        public string cityName {get;set;}
+        public  string cityCode{get;set;}
+    }
+
+    public class AreaItem
+    {
+        public int id { get; set; }
+        public string areaName { get; set; }
+        public string code { get; set; }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public override string ToString()
+        {
+            return name;
         }
     }
 }
