9f61981333541cf8dbfa4be72e2f98a41121a1a7
wdwoodee <wdwoodee@163.com>
2016-06-10 11:52:52 +0800

    modify

--- ADONET/SQLConnection/StrongDataSet/Form1.cs
+++ ADONET/SQLConnection/StrongDataSet/Form1.cs
@@ -21,14 +21,16 @@ namespace StrongDataSet
         private void btnStrongDS_Click(object sender, EventArgs e)
         {
             T_PersonTableAdapter adapter = new T_PersonTableAdapter();
-            StrongDataSet.DataSet.T_PersonDataTable persons = adapter.GetData();
-            for (int i = 0; i < persons.Count; i++)
+            StrongDataSet.DataSet.T_PersonDataTable personTable = adapter.GetData();
+            for (int i = 0; i < personTable.Count; i++)
             {
-                StrongDataSet.DataSet.T_PersonRow person = persons[i];
+                StrongDataSet.DataSet.T_PersonRow person = personTable[i];
                 string msg = string.Format("Name: {0}, Age: {1}", person.Name, person.Age);
                 MessageBox.Show(msg);
             }
-        
+            //personTable[0].Name = "aaa";
+            //adapter.Update(personTable);
+            //adapter.Insert("xiaoli", 50);
         }
     }
 }
