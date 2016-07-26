016f182f0962b13a645d4237bd3464475a4a4fb8
wdwoodee <wdwoodee@163.com>
2016-07-27 00:14:55 +0800

    add all

---
+++ ADONET/CommonTest/CommonTest/LambdaTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTest
+{
+    public class LambdaTest
+    {
+        //传统的调用委托的示例##############  
+        public static void FindListDelegate()
+        {
+            //先创建一个泛型的List类    
+            List<string> list = new List<string>();
+            list.AddRange(new string[] { "ASP.NET课程", "J2EE课程", "PHP课程", "数据结构课程" });
+            Predicate<string> findPredicate = new Predicate<string>(IsBookCategory);
+            List<string> bookCategory = list.FindAll(findPredicate);
+            foreach (string str in bookCategory)
+            {
+                Console.WriteLine("{0}\t", str);
+            }
+        }
+        //谓词方法，这个方法将被传递给FindAll方法进行书书籍分类的判断    
+        public static bool IsBookCategory(string str)
+        {
+            return str.EndsWith("课程") ? true : false;
+        }
+
+        //使用匿名方法来进行搜索过程##############  
+        public static void FindListAnonymousMethod()
+        {
+            //先创建一个泛型的List类    
+            List<string> list = new List<string>();
+            list.AddRange(new string[] { "ASP.NET课程", "J2EE课程", "PHP课程", "数据结构课程" });
+            //在这里，使用匿名方法直接为委托创建一个代码块，而不用去创建单独的方法    
+            List<string> bookCategory = list.FindAll(delegate(string str)
+            {
+                return str.EndsWith("课程") ? true : false;
+            });
+            foreach (string str in bookCategory)
+            {
+                Console.WriteLine("{0}\t", str);
+            }
+        }
+
+        //使用Lambda来实现搜索过程##############   
+        public static void FindListLambdaExpression()
+        {
+            //先创建一个泛型的List类    
+            List<string> list = new List<string>();
+            list.AddRange(new string[] { "ASP.NET课程", "J2EE课程", "PHP课程", "数据结构课程" });
+            //在这里，使用了Lambda来创建一个委托方法    
+            List<string> bookCategory = list.FindAll((string str) => str.EndsWith("课程"));
+            foreach (string str in bookCategory)
+            {
+                Console.WriteLine("{0}\t", str);
+            }
+        }    
+    }
+}
---
+++ ADONET/CommonTest/DelagateTest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelagateTest
+{
+    //步骤：1、声明一个delegate类型
+    public delegate string MyDelegate(string name);
+    public class Program
+    {
+        //2、定义准备被委托调用的方法
+        public static string FunctionA(string name)
+        {
+            return "A say hello to " + name;
+        }
+
+        public static string FunctionB(string name)
+        {
+            return "B say hello to " + name;
+        }
+        
+        //3、定义delegate类型的处理函数，并在此函数中通过delegate类型调用定义的方法
+        public static void MethodA(MyDelegate me)
+        {
+            Console.WriteLine(me("zhangsan"));
+        }
+
+
+        static void Main(string[] args)
+        {
+            //4、创建实例，传入准备调用的方法名
+            MyDelegate a = new MyDelegate(FunctionA);
+            MyDelegate b = new MyDelegate(FunctionB);
+            MethodA(a);
+            MethodA(b);
+
+            TestEvent myEvent = new TestEvent();
+            myEvent.Click += new TestEvent.MyEventHandler(myEvent.M1);
+            myEvent.Click += new TestEvent.MyEventHandler(myEvent.M2);
+            myEvent.Click += new TestEvent.MyEventHandler(myEvent.M3);
+            myEvent.Trigger();
+            myEvent.Click -= new TestEvent.MyEventHandler(myEvent.M3);
+            myEvent.Trigger();
+        }
+    }
+
+    public class TestEvent
+    {
+        //定义事件的签名，为事件定义委托
+        public delegate void MyEventHandler();
+        
+        //用event来声明事件
+        public event MyEventHandler Click;
+        
+        //定义引发该事件时要调用的方法
+        public void Trigger()
+        {
+            Click();
+        }
+
+        public void M1()
+        {
+            Console.WriteLine("Hello from m1");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("Hello from m2");
+        }
+
+        public void M3()
+        {
+            Console.WriteLine("Hello from m3");
+        }
+    }
+}
