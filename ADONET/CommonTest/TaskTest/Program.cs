bf1a71fba7a8f815832bb760926a438dbcaa7352
wdwoodee <wdwoodee@163.com>
2016-07-28 23:33:33 +0800

    add tasktest

---
+++ ADONET/CommonTest/TaskTest/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Create Task
+            //var task1 = new Task(() =>
+            //{
+            //    Console.WriteLine("Hello,task");
+            //});
+            //task1.Start();
+
+            //var task2 = Task.Factory.StartNew(() =>
+            //{
+            //    Console.WriteLine("Hello,task started by task factory");
+            //});
+            #endregion
+
+            #region status
+            //task1.Wait();就是等待任务执行完成，我们可以看到最后task1的状态变为Completed。
+            //var task1 = new Task(() =>
+            //{
+            //    Console.WriteLine("Begin");
+            //    System.Threading.Thread.Sleep(2000);
+            //    Console.WriteLine("Finish");
+            //});
+            //Console.WriteLine("Before start:" + task1.Status);
+            //task1.Start();
+            //Console.WriteLine("After start:" + task1.Status);
+            //task1.Wait();
+            //Console.WriteLine("After Finish:" + task1.Status);
+            #endregion
+
+            #region waitall
+
+            //waitall就是等待所有的任务都执行完成
+            //var task1 = new Task(() =>
+            //{
+            //    Console.WriteLine("Task 1 Begin");
+            //    System.Threading.Thread.Sleep(2000);
+            //    Console.WriteLine("Task 1 Finish");
+            //});
+            //var task2 = new Task(() =>
+            //{
+            //    Console.WriteLine("Task 2 Begin");
+            //    System.Threading.Thread.Sleep(3000);
+            //    Console.WriteLine("Task 2 Finish");
+            //});
+
+            //task1.Start();
+            //task2.Start();
+            ////Task.WaitAll(task1, task2);
+
+            ////等待任何一个任务完成就继续向下执行
+            //Task.WaitAny(task1, task2);
+            //Console.WriteLine("All task finished!");
+
+
+            #endregion
+
+            #region ContinueWith
+            //ContinueWith 就是在第一个Task完成后自动启动下一个Task，实现Task的延续
+            //var task1 = new Task(() =>
+            //{
+            //    Console.WriteLine("Task 1 Begin");
+            //    System.Threading.Thread.Sleep(2000);
+            //    Console.WriteLine("Task 1 Finish");
+            //});
+            //var task2 = new Task(() =>
+            //{
+            //    Console.WriteLine("Task 2 Begin");
+            //    System.Threading.Thread.Sleep(3000);
+            //    Console.WriteLine("Task 2 Finish");
+            //});
+
+
+            //task1.Start();
+            //task2.Start();
+            //var result = task1.ContinueWith<string>(task =>
+            //{
+            //    Console.WriteLine("task1 finished!");
+            //    return "This is task result!";
+            //});
+
+            //Console.WriteLine(result.Result.ToString());
+            #endregion
+
+            #region 
+            //在每次调用ContinueWith方法时，每次会把上次Task的引用传入进来，以便检测上次Task的状态，比如我们可以使用上次Task的Result属性来获取返回值。
+            //var SendFeedBackTask = Task.Factory.StartNew(() => { Console.WriteLine("Get some Data!"); })
+            //                .ContinueWith<bool>(s => { return true; })
+            //                .ContinueWith<string>(r =>
+            //                {
+            //                    if (r.Result)
+            //                    {
+            //                        return "Finished";
+            //                    }
+            //                    else
+            //                    {
+            //                        return "Error";
+            //                    }
+            //                });
+            //Console.WriteLine(SendFeedBackTask.Result);
+
+            //其实上面的写法简化一下，可以这样写：
+            Task.Factory.StartNew<string>(() => { return "One"; }).ContinueWith(ss => { Console.WriteLine(ss.Result); });
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
--- ADONET/CommonTest/TheadTest/Program.cs
+++ ADONET/CommonTest/TheadTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,99 @@ namespace TheadTest
 {
     public class Program
     {
+        private static Stopwatch stopWatch = new Stopwatch();
         static void Main(string[] args)
         {
+            //Parallel.For(0, 100, i =>
+            //{
+            //    Console.Write(i + "\t");
+            //});
+
+            ParallelTest paraTest = new ParallelTest();
+            //paraTest.ParallelBreak();
+            paraTest.TestPLinq();
+            
+            ParallelDemo paraDemo = new ParallelDemo();
+            paraDemo.ParallelInvokeMethod();
+
+            //DateTime startTime;
+            //TimeSpan resultTime;
+            //List<entityA> source = new List<entityA>();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    source.Add(new entityA
+            //    {
+            //        name = "悟空" + i,
+            //        sex = i % 2 == 0 ? "男" : "女",
+            //        age = i
+            //    });
+            //}
+            //startTime = System.DateTime.Now;
+            //stopWatch.Start();
+            //loop1(source);
+            //stopWatch.Stop();
+            //Console.WriteLine("一般for循环耗时： " + stopWatch.ElapsedMilliseconds + " ms.");
+            //resultTime = System.DateTime.Now - startTime;
+            //Console.WriteLine("一般for循环耗时：" + resultTime);
+            //startTime = System.DateTime.Now;
+            //loop2(source);
+            //resultTime = System.DateTime.Now - startTime;
+            //Console.WriteLine("一般foreach循环耗时：" + resultTime);
+            //startTime = System.DateTime.Now;
+            //loop3(source);
+            //resultTime = System.DateTime.Now - startTime;
+            //Console.WriteLine("并行for循环耗时：" + resultTime.Milliseconds);
+            //startTime = System.DateTime.Now;
+            //loop4(source);
+            //resultTime = System.DateTime.Now - startTime;
+            //Console.WriteLine("并行foreach循环耗时：" + resultTime.Milliseconds);
+            //Console.ReadLine();
         }
+
+        //普通的for循环
+        static void loop1(List<entityA> source)
+        {
+            int count = source.Count();
+            for (int i = 0; i < count; i++)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        //普通的foreach循环
+        static void loop2(List<entityA> source)
+        {
+            foreach (entityA item in source)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        //并行的for循环
+        static void loop3(List<entityA> source)
+        {
+            int count = source.Count();
+            Parallel.For(0, count, item =>
+            {
+                System.Threading.Thread.Sleep(100);
+            });
+        }
+
+        //并行的foreach循环
+        static void loop4(List<entityA> source)
+        {
+            Parallel.ForEach(source, item =>
+            {
+                System.Threading.Thread.Sleep(100);
+            });
+        }
+    }
+
+    //简单的实体
+    class entityA
+    {
+        public string name { set; get; }
+        public string sex { set; get; }
+        public int age { set; get; }
     }
 }
