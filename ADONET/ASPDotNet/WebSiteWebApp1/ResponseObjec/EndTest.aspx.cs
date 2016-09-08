098349c4ece72d12b1de9a8d39bf3787dc87e373
wdwoodee <wdwoodee@163.com>
2016-09-08 23:31:25 +0800

    add new file Hello.aspx

---
+++ ADONET/ASPDotNet/WebSiteWebApp1/ResponseObjec/EndTest.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ResponseObjec_EndTest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string q = Request["q"];
+        if (q == "1")
+        {
+            Response.Write("One");
+        }
+        else if (q == "2")
+        {
+            //此种是server内部的跳转，页面url不会改变，只发出一个请求，并且该跳转至的页面能够获取到参数
+            Server.Transfer("Hello.aspx");
+        }
+        else if (string.IsNullOrEmpty(q))
+        {
+            Response.Write("请输入问题");
+        }
+        else
+        {
+            //与Transfer刚好相反
+            Response.Redirect("Hello.aspx");
+            Response.Redirect("http://www.baidu.com");
+        }
+    }
+}
---
+++ ADONET/ASPDotNet/WebSiteWebApp1/ResponseObjec/Hello.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Hello.aspx.cs" Inherits="ResponseObjec_Hello" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Hello my friend.
+    </div>
+    </form>
+</body>
+</html>
