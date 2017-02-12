a650bd35967f5c5e02c3274686a43986654c01db
wdwoodee <wdwoodee@163.com>
2017-02-12 17:39:49 +0800

    add common project

---
+++ vs2013/CommonTest/common/GouZao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GouZao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b1 = new B();
+            B b2 = new B(10);
+            Console.ReadKey();
+        }
+    }
+
+    public class A
+    {
+        //未赋值，初始化为0
+        protected int age;
+        public A()
+        {
+            Console.WriteLine("A: {0}", age);
+        }
+
+        public A(int age)
+        {
+            this.age = age;
+            Console.WriteLine("A: {0}", age);
+        }
+    }
+
+    public class B : A
+    {
+        public B() : base()
+        {
+            Console.WriteLine("B: {0}", age);
+        }
+
+         public B(int age) : base(age)
+        {
+            age = 5;
+            Console.WriteLine("B: {0}", age);
+        }
+    }
+}
---
+++ vs2013/CommonTest/common/common/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Eat();
+            animal.Talk();
+            Console.WriteLine("base\r\n");
+
+            Cat cat = new Cat();
+            cat.Walk();
+            cat.Eat();
+            cat.Talk();
+            Console.WriteLine("derived\r\n");
+
+            //限定父类的行为和数据
+            Animal catAnimal = new Cat();            
+            catAnimal.Eat();
+
+            //重写后，调用的子类方法
+            catAnimal.Talk();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Base: Hello, Animal!");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Base: Eating!");
+        }
+
+        public virtual void Talk()
+        {
+            Console.WriteLine("Base: Talking!");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat()
+        {
+            Console.WriteLine("Derived: Hello, Cat!");
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("Derived: Walk!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Derived: Eating!");
+        }
+
+        public override void Talk()
+        {
+            Console.WriteLine("Derived: Talking!");
+        }
+    }
+}
